7925fc8a2c174c175b89a8d001b8b88be957de0b
Hien Vo <hienvo310799@gmail.com>
2021-10-16 16:33:05 +0700

    lesson3

---
+++ project3/src/main.java
@@ -0,0 +1,55 @@
+public class main {
+    public static void main(String[] args) {
+
+
+     /*
+     Scanner sc = new Scanner(System.in);
+        System.out.println("Nhập chuỗi: ");
+        String str2 = sc.nextLine(); //Trả về nội dung của chuỗi nhập vào
+
+        System.out.println("Nhập dữ liệu kiểu byte: ");
+        byte byteNumber = sc.nextByte(); //Trả về dữ liệu kiểu byte
+
+        System.out.println("Nhập dữ liệu kiểu short: ");
+        short shortNumber = sc.nextShort(); //Trả về dữ liệu kiểu short
+
+        System.out.println("Nhập dữ liệu kiểu int: ");
+        int intNumber = sc.nextInt(); //Trả về dữ liệu kiểu int
+
+        System.out.println("Nhập dữ liệu kiểu long: ");
+        long longNumber = sc.nextLong();  //Trả về dữ liệu kiểu long
+
+        System.out.println("Nhập dữ liệu kiểu float: ");
+        float floatNumber = sc.nextFloat();  //Trả về dữ liệu kiểu float
+
+        System.out.println("Nhập dữ liệu kiểu double: ");
+        double doubleNumber = sc.nextDouble();  //Trả về dữ liệu kiểu double
+
+
+        //In thông tin
+
+        System.out.println("In chuỗi với nextLine(): "+str2);
+        System.out.println("In số kiểu byte: "+byteNumber);
+        System.out.println("In số kiểu short: "+shortNumber);
+        System.out.println("In số kiểu int: "+intNumber);
+        System.out.println("In số kiểu long: "+longNumber);
+        System.out.println("In số kiểu float: "+floatNumber);
+        System.out.println("In số kiểu double: "+doubleNumber);
+
+        sc.close();
+
+      */
+        int i =2 ;
+        int j=7;
+        System.out.printf("\nTổng của của %d và %d là %d",i,j,i+j);
+        double h = 8;
+        double k = 3;
+        System.out.printf("\nCó giá trị:%f",h);
+        System.out.printf("\nThương của:%.1f và %.1f là %.2f",h,k,h/k);
+
+        char c='a';
+        System.out.printf("\nký tự: %c",c);
+    }
+
+
+}
